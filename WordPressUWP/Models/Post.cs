6315e58a1a8050b7a77c62089c324316f6671355
Thomas Pentenrieder <pentenrieder@outlook.com>
2016-03-23 01:42:22 +0100

    first working request for post list

---
+++ WordPressUWP/Models/Post.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressUWP.Models
+{
+    public class Post
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("date")]
+        public DateTime date { get; set; }
+        [JsonProperty("date_gmt")]
+        public DateTime date_gmt { get; set; }
+        [JsonProperty("guid")]
+        public Guid guid { get; set; }
+        public string modified { get; set; }
+        public string modified_gmt { get; set; }
+        public string slug { get; set; }
+        public string type { get; set; }
+        public string link { get; set; }
+        public Title title { get; set; }
+        public Content content { get; set; }
+        public Excerpt excerpt { get; set; }
+        public int author { get; set; }
+        public int featured_image { get; set; }
+        public string comment_status { get; set; }
+        public string ping_status { get; set; }
+        public bool sticky { get; set; }
+        public string format { get; set; }
+        //public Links _links { get; set; }
+
+    }
+    public class Guid
+    {
+        public string rendered { get; set; }
+    }
+
+    public class Title
+    {
+        public string rendered { get; set; }
+    }
+
+    public class Content
+    {
+        public string rendered { get; set; }
+    }
+
+    public class Excerpt
+    {
+        public string rendered { get; set; }
+    }
+
+    public class Self
+    {
+        public string href { get; set; }
+    }
+
+    public class Collection
+    {
+        public string href { get; set; }
+    }
+
+    public class About
+    {
+        public string href { get; set; }
+    }
+
+    public class Author
+    {
+        public bool embeddable { get; set; }
+        public string href { get; set; }
+    }
+
+    public class Reply
+    {
+        public bool embeddable { get; set; }
+        public string href { get; set; }
+    }
+
+    public class VersionHistory
+    {
+        public string href { get; set; }
+    }
+
+    public class HttpsApiWOrgFeaturedmedia
+    {
+        public bool embeddable { get; set; }
+        public string href { get; set; }
+    }
+
+    public class HttpsApiWOrgAttachment
+    {
+        public string href { get; set; }
+    }
+
+    public class HttpsApiWOrgTerm
+    {
+        public string taxonomy { get; set; }
+        public bool embeddable { get; set; }
+        public string href { get; set; }
+    }
+
+    public class HttpsApiWOrgMeta
+    {
+        public bool embeddable { get; set; }
+        public string href { get; set; }
+    }
+
+}
---
+++ WordPressUWP/WordPressClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordPressUWP.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace WordPressUWP
+{
+    public class WordPressClient
+    {
+        public String Endpoint { get; set; }
+
+
+        public async Task<List<Post>> ListPosts()
+        {
+            List<Post> result = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Endpoint);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync("posts");
+                if (response.IsSuccessStatusCode)
+                {
+                    Debug.WriteLine("success");
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<List<Post>>(responseString);
+                }
+            }
+            return result;
+
+        }
+
+    }
+}
