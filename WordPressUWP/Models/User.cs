1036ab4fde439bcdac1fe8754243fab0c5a172c6
Thomas Pentenrieder <pentenrieder@outlook.com>
2016-03-23 13:35:15 +0100

    added user model

---
+++ WordPressUWP/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordPressUWP.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string description { get; set; }
+        public string link { get; set; }
+        public AvatarUrls avatar_urls { get; set; }
+        public string slug { get; set; }
+        public Links _links { get; set; }
+    }
+
+    public class AvatarUrls
+    {
+        [JsonProperty("24")]
+        public string size24 { get; set; }
+        [JsonProperty("48")]
+        public string size48 { get; set; }
+        [JsonProperty("96")]
+        public string size96 { get; set; }
+    }
+
+    public class Links
+    {
+        public List<Self> self { get; set; }
+        public List<Collection> collection { get; set; }
+    }
+
+}
--- WordPressUWP/WordPressClient.cs
+++ WordPressUWP/WordPressClient.cs
@@ -82,5 +82,6 @@ namespace WordPressUWP
         }
 
 
+
     }
 }
