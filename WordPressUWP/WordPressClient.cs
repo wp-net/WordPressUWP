642cc8436d205b8015babf0f30cbc190e45649f3
Thomas Pentenrieder <pentenrieder@outlook.com>
2016-03-23 21:10:54 +0100

    merge

--- WordPressUWP/WordPressClient.cs
+++ WordPressUWP/WordPressClient.cs
@@ -35,27 +35,30 @@ namespace WordPressUWP
 			get { return _endpoint; }
 		}
 
-		public async Task<IList<Post>> ListPosts(int page = 1, int per_page = 10, int offset = 0, Post.OrderBy orderby = Post.OrderBy.date)
+        public String Username { get; set; }
+        public String Password { get; set; }
+
+        public async Task<IList<Post>> ListPosts(int page = 1, int per_page = 10, int offset = 0, Post.OrderBy orderby = Post.OrderBy.date)
 		{
-			return await Download<Post[]>($"posts").ConfigureAwait(false);
+			return await Download<Post[]>($"posts", false).ConfigureAwait(false);
 		}
 
 		public async Task<Post> GetPost(String id)
 		{
-			return await Download<Post>($"posts/{id}").ConfigureAwait(false);
+			return await Download<Post>($"posts/{id}", false).ConfigureAwait(false);
 		}
 
 		public async Task<IList<Comment>> ListComments()
 		{
-			return await Download<Comment[]>("comments").ConfigureAwait(false);
+			return await Download<Comment[]>("comments", false).ConfigureAwait(false);
 		}
 
 		public async Task<Comment> GetComment(string id)
 		{
-			return await Download<Comment>($"comment/{id}").ConfigureAwait(false);
+			return await Download<Comment>($"comment/{id}", false).ConfigureAwait(false);
 		}
 
-		protected async Task<TClass> Download<TClass>(string section)
+		protected async Task<TClass> Download<TClass>(string section, bool isAuthRequired)
 			where TClass : class
 		{
 			using (var client = new HttpClient())
@@ -69,5 +72,22 @@ namespace WordPressUWP
 			}
 			return default(TClass);
 		}
-	}
+
+        public async Task<User> GetCurrentUser()
+        {
+            User result = null;
+            var basicauth = "Basic " + Utility.Authentication.Base64Encode($"{Username}:{Password}");
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicauth);
+                var response = await client.GetAsync($"{Endpoint}users/me");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<User>(responseString);
+                }
+            }
+            return result;
+        }
+    }
 }
