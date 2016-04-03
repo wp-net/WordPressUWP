a1f5e1ff3a3f4222c1c233de5efc1a1ae30dbb59
Thomas Pentenrieder <pentenrieder@outlook.com>
2016-04-04 01:48:26 +0200

    added test app project

---
+++ WordPressUWPTestApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WordPressUWP;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WordPressUWPTestApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            TestPosts();
+        }
+
+        public async void TestPosts()
+        {
+            var client = new WordPressClient(ApiCredentials.WordPressUri);
+            client.Username = ApiCredentials.Username;
+            client.Password = ApiCredentials.Password;
+
+            var posts = await client.ListPosts();
+            var post = await client.GetPost("1");
+           
+            var comments = await client.ListComments();
+            var comment = await client.GetComment("3");
+
+            //var currentUser = await client.GetCurrentUser();
+        }
+    }
+}
