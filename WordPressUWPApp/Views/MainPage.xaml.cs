121e2feeb583d61188429f166bf0a4952832c997
Thomas Pentenrieder <pentenrieder@outlook.com>
2016-04-12 19:06:03 +0200

    initial WordPressUWPApp, based on Template10 Hamburger

---
+++ WordPressUWPApp/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using WordPressUWPApp.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace WordPressUWPApp.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
