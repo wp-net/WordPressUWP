716f71294deb7ae350cec99f016b5ef7cadbb464
Thomas Pentenrieder <pentenrieder@hotmail.com>
2017-02-14 12:48:21 +0100

    Nuget Updates
    Updated UWP Packages
    Replaced WebView DependencyProperty in favor of UWP Toolkit
    implementation

--- WordPressUWPApp/Utility/HtmlTools.cs
+++ WordPressUWPApp/Utility/HtmlTools.cs
@@ -15,44 +15,5 @@ namespace WordPressUWPApp.Utility
         {
             return Regex.Replace(text, @"<(.|\n)*?>", string.Empty);
         }
-
-
-        public static string GetContent(DependencyObject obj)
-        {
-            return (string)obj.GetValue(ContentProperty);
-        }
-
-
-        public static void SetContent(DependencyObject obj, string value)
-        {
-            obj.SetValue(ContentProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for HTML.  This enables animation, styling, binding, etc...  
-        public static readonly DependencyProperty ContentProperty = DependencyProperty.RegisterAttached(
-              "Content", 
-              typeof(string), 
-              typeof(HtmlTools), 
-              new PropertyMetadata(string.Empty, OnContentChanged));
-
-
-        //private static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        //{
-        //    WebView wv = d as WebView;
-        //    if (wv != null)
-        //    {
-        //        wv.NavigateToString((string)e.NewValue);
-        //    }
-        //}
-        private static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            WebView wv = d as WebView;
-            var content = e.NewValue as string;
-            if (string.IsNullOrEmpty(content))
-            {
-                return;
-            }
-            wv?.NavigateToString(content);
-        }
     }
 }
