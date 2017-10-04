e3f98351e0c4971e4cbf9effdaa87c065f52710f
Thomas Pentenrieder <pentenrieder@medialesson.de>
2017-10-04 09:11:34 +0200

    add threaded comments helper

---
+++ WordPressUWP/Models/CommentThreaded.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordPressPCL.Models;
+
+namespace WordPressUWP.Models
+{
+    public class CommentThreaded : Comment
+    {
+        public int Depth { get; set; }
+    }
+}
