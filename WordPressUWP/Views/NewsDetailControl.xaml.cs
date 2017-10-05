88f22ef2558b83c09e1a9b136bbb64ff9b161e12
Thomas Pentenrieder <pentenrieder@medialesson.de>
2017-10-05 10:54:23 +0200

    fix style

--- WordPressUWP/Views/NewsDetailControl.xaml.cs
+++ WordPressUWP/Views/NewsDetailControl.xaml.cs
@@ -26,22 +26,22 @@ namespace WordPressUWP.Views
             if (args.Uri == null)
                 return;
             var s = args.Uri.ToString();
-            if (s.Contains(".jpg") || s.Contains(".jpeg") || s.Contains(".png") || s.Contains(".gif"))
-            {
-                args.Cancel = true;
-                Debug.WriteLine(s);
-                //WindowWrapper.Current().Dispatcher.Dispatch(() =>
-                //{
-                //    var modal = Window.Current.Content as ModalDialog;
-                //    modal.CanBackButtonDismiss = true;
-                //    modal.DisableBackButtonWhenModal = false;
-                //    var view = modal.ModalContent as Busy;
-                //    if (view == null)
-                //        modal.ModalContent = view = new Busy();
-                //    modal.IsModal = view.IsBusy = true;
-                //    view.BusyText = "text";
-                //});
-            }
+            //if (s.Contains(".jpg") || s.Contains(".jpeg") || s.Contains(".png") || s.Contains(".gif"))
+            //{
+            //    args.Cancel = true;
+            //    Debug.WriteLine(s);
+            //    //WindowWrapper.Current().Dispatcher.Dispatch(() =>
+            //    //{
+            //    //    var modal = Window.Current.Content as ModalDialog;
+            //    //    modal.CanBackButtonDismiss = true;
+            //    //    modal.DisableBackButtonWhenModal = false;
+            //    //    var view = modal.ModalContent as Busy;
+            //    //    if (view == null)
+            //    //        modal.ModalContent = view = new Busy();
+            //    //    modal.IsModal = view.IsBusy = true;
+            //    //    view.BusyText = "text";
+            //    //});
+            //}
         }
 
         private void WebView_UnsafeContentWarningDisplaying(WebView sender, object args)
