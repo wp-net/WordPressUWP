aa3923941c1d69d0c8f5a3dcb8102422224a6bd0
Thomas Pentenrieder <pentenrieder@outlook.com>
2017-10-06 23:36:17 +0200

    fix news details style

--- WordPressUWP/ViewModels/ShellViewModel.cs
+++ WordPressUWP/ViewModels/ShellViewModel.cs
@@ -189,7 +189,7 @@ namespace WordPressUWP.ViewModels
             // More on Segoe UI Symbol icons: https://docs.microsoft.com/windows/uwp/style/segoe-ui-symbol-font
             // Or to use an IconElement instead of a Symbol see https://github.com/Microsoft/WindowsTemplateStudio/blob/master/docs/projectTypes/navigationpane.md
             // Edit String/en-US/Resources.resw: Add a menu item title for each page
-            _primaryItems.Add(new ShellNavigationItem("Shell_News".GetLocalized(), Symbol.Document, typeof(NewsViewModel).FullName));
+            _primaryItems.Add(new ShellNavigationItem("Shell_News".GetLocalized(), Symbol.Home, typeof(NewsViewModel).FullName));
             _secondaryItems.Add(new ShellNavigationItem("Shell_Settings".GetLocalized(), Symbol.Setting, typeof(SettingsViewModel).FullName));
             _secondaryItems.Add(new ShellNavigationItem("Me", Symbol.Contact, "Login"));
         }
