e71e6dad4b6a1e7f975439dec9d7701f54be9c3a
Thomas Pentenrieder <pentenrieder@hotmail.com>
2017-10-22 01:34:56 +0200

    responsive comments

--- WordPressUWP/Views/NewsPage.xaml.cs
+++ WordPressUWP/Views/NewsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -22,6 +23,28 @@ namespace WordPressUWP.Views
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             await ViewModel.Init(WindowStates.CurrentState);
+            Window.Current.SizeChanged += Current_SizeChanged;
+        }
+
+        private void Current_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            if(Window.Current.Bounds.Width >= 1300)
+            {
+                CommentsColumn.Margin = new Thickness(0, 0, 0, 0);
+                CommentToggleButton.IsChecked = false;
+            }
+        }
+
+        private void CommentToggleButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            bool showComments = CommentToggleButton.IsChecked ?? false;
+            if (showComments)
+            {
+                CommentsColumn.Margin = new Thickness(-400, 0, 0, 48);
+            } else
+            {
+                CommentsColumn.Margin = new Thickness(0, 0, -400, 0);
+            }
         }
     }
 }
