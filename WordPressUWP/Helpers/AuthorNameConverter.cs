c1742eb4956f01a7bbd9f837a55dcc4a91c63a02
Thomas Pentenrieder <pentenrieder@hotmail.com>
2018-01-20 22:02:55 +0100

    News List redesign

---
+++ WordPressUWP/Helpers/AuthorNameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Data;
+using WordPressPCL.Models;
+
+namespace WordPressUWP.Helpers
+{
+    public class AuthorNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if(value is List<User> authors)
+            {
+                return authors[0].Name;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
